b6487941a935f604440d4a212490d4d6c13f7feb
Leonardo Rodrigues <leordoria2016@gmail.com>
2025-02-07 17:03:48 -0300

    Routes settings/Navbar Component/Footer Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/templates/navbar/navbar.component';
+import { FooterComponent } from './components/templates/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
